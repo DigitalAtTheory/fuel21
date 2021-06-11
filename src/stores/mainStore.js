c0422d4bc88c2a038b417a8377180520b3638563
Josh Suson <joshsuson@Joshs-Mac-mini.local>
2021-06-11 14:55:45 -0400

    trying to fix images

--- src/stores/mainStore.js
+++ src/stores/mainStore.js
@@ -4,27 +4,27 @@ const issueData = [
 	{
 		name: 'Issue 1',
 		slug: `issue-1`,
-		issue: `../src/assets/FUEL-2021-JANUARY-DESKTOP-HANDOFF.jpg`
+		issue: `https://res.cloudinary.com/joshsuson/image/upload/v1623437139/Fuel%2021/FUEL-2021-JANUARY-DESKTOP-HANDOFF_bxgqlf.jpg`
 	},
 	{
 		name: 'Issue 2',
 		slug: `issue-2`,
-		issue: `../src/assets/FUEL-FEB-2021-DESKTOP-HANDOFF.jpg`
+		issue: `https://res.cloudinary.com/joshsuson/image/upload/v1623437139/Fuel%2021/FUEL-FEB-2021-DESKTOP-HANDOFF_uget4j.jpg`
 	},
 	{
 		name: 'Issue 3',
 		slug: `issue-3`,
-		issue: `../src/assets/FUEL-MARCH-2021-DESKTOP.jpg`
+		issue: `https://res.cloudinary.com/joshsuson/image/upload/v1623437139/Fuel%2021/FUEL-MARCH-2021-DESKTOP_g5vqxg.jpg`
 	},
 	{
 		name: 'Issue 4',
 		slug: `issue-4`,
-		issue: `../src/assets/FUEL-APRIL-2021-HANDOFF-DESKTOP.jpg`
+		issue: `https://res.cloudinary.com/joshsuson/image/upload/v1623437139/Fuel%2021/FUEL-APRIL-2021-HANDOFF-DESKTOP_nayp15.jpg`
 	},
 	{
 		name: 'Issue 5',
 		slug: `issue-5`,
-		issue: `../src/assets/2021-FUEL-MAY-HANDOFF-DESKTOP.jpg`
+		issue: `https://res.cloudinary.com/joshsuson/image/upload/v1623437139/Fuel%2021/2021-FUEL-MAY-HANDOFF-DESKTOP_srkmih.jpg`
 	}
 ];
 
